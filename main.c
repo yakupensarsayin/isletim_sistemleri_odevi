d592fc74f3e39ba986c34d01ad213e8423927dfc
Sude Kocaacar <sudekocaacar@gmail.com>
2024-11-29 11:30:00 +0300

    execvp calistiktan sonra ebeveyne don

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
     char *args[] = {"ls", "-la", NULL};
-    execvp(args[0], args);
+
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        execvp(args[0], args);
+    }
+    else if (pid > 0)
+    {
+        wait(NULL);
+        printf("Ana proses devraldi.");
+    }
+
     exit(EXIT_SUCCESS);
 }
